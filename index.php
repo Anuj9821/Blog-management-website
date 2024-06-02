0396ee9f87cc30b9ac901b63b6bb11d764384cbe
Anuj9821 <walsepatilanuj9821@gmail.com>
2024-06-02 22:10:04 +0530

    login page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP Page</title>
+</head>
+<body>
+    <h1>Welcome to my simple PHP page!</h1>
+    <p><?php echo "Hello, World!"; ?></p>
+    <p>Current Date and Time: <?php echo date('Y-m-d H:i:s'); ?></p>
+</body>
+</html>
